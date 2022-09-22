0e1ac9cf23cc40a0e22bca8e3116a58dadb55b74
Vladyslav Komar <valdek125@gmail.com>
2022-09-23 01:36:26 +0300

    npm i

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,36 @@
 import { GlobalStyle } from './GlobalStyle';
-import { Component } from 'react';
+import { Component, useState } from 'react';
 import typeOfFB from '../feedbackTypes'
 import { Feedback } from './Feedback/Feedback';
 import { Statistics } from './Statistics/Statistics';
 
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  }
+export const App = () => {
+  const [good, setGood] = useState(0)
+  
+  // state = {
+  //   good: 0,
+  //   neutral: 0,
+  //   bad: 0
+  // }
 
-  addFeedback = feedback => this.setState(prevState =>
+  const addFeedback = feedback => this.setState(prevState =>
     ({ [feedback]: prevState[feedback] + 1 }));
 
-  render() {
     return (
       <>
         <Feedback
           title="Please leave feedback"
           btnNames={ typeOfFB }
-          addFeedback={ this.addFeedback }
+          addFeedback={ addFeedback }
         />
         <Statistics
           title="Statistics"
           stats={typeOfFB}
-          state={this.state}
+          state={state}
         />
         <GlobalStyle />
       </>
     );
-  }
+  
 };
